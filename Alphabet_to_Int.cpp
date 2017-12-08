28fb3b81a2f50df849b423ddb41aec66a0c32fb6
me <a>
2017-12-09 04:18:38 +0900

    added

--- Alphabet_to_Int.cpp
+++ Alphabet_to_Int.cpp
@@ -15,15 +15,37 @@ class Alpha_to_Int {
 public:
 	void Init_Alpha_to_Int() {
 		Alpha_str = "one;three;six;nine;eight;two;two;one;";
+		//アルファベットで示される数値は;区切りで渡される
 		ConvertAtoI();
 
 	}
 	void ConvertAtoI() {
 		int i = 0;
 		while (*(Alpha_str.c_str() + i) != '\0') {
-			if (Alpha_str.find("one")!=string::npos)printf("%s\n", Alpha_str.c_str());
-			i++;
+			int j = 0; 
+			int word_end;
+			
+			while (*(Alpha_str.c_str() + i+j)!=';')//次の区切りを探す
+			{
+				word_end = j;
+				j++;
+			}
+			string number = Alpha_str.substr(i,word_end+1);//;と;の間をnumberに渡す
+
+
+			if (number == "one")printf("1");
+			if (number == "two")printf("2");
+			if (number == "three")printf("3");
+			if (number == "four")printf("4");
+			if (number == "five")printf("5");
+			if (number == "six")printf("6");
+			if (number == "seven")printf("7");
+			if (number == "eight")printf("8");
+			if (number == "nine")printf("9");
+			if (number == "zero")printf("0");
+			i+=j+1;
 		}
+		puts("");
 	}
 
 
---
+++ Knapsack.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <queue>
+#include <cmath>
+using namespace std;
+class Knapsack {
+
+
+	int N, W;
+	vector<int> v, w;
+public:
+	Knapsack() {
+		scanf("%d %d",&N,&W);
+
+		for (int i = 0; i < N; i++) {
+			int a, b;
+			scanf("%d %d", &a, &b);
+			v.push_back(a);
+			w.push_back(b);
+
+		}
+		vector<bool> bit(N);
+
+		//ëSêîíTçı
+		for (int i = 0; i < pow(2, N) - 1; i++) {
+			int j = N - 1;
+
+			if (!bit[j])bit[j] = true;//binary adder
+			else {
+				bit[j] = false;
+				while (bit[j]) {
+					if (bit[j]) {
+						bit[j] = false;
+						j--;
+						continue;
+					}
+					else bit[j] = true;
+				}
+			}
+
+		}
+
+
+	}
+
+};
