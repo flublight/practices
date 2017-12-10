6c7340f525bd6bf06d0f6c08df385660ae67411b
me <a>
2017-12-11 02:43:49 +0900

    Atcoder ABC081

---
+++ ABC081C.cpp
@@ -0,0 +1,67 @@
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+class ABC081C {
+
+	int N, K;
+public:
+
+	ABC081C() {
+		cin >> N;
+		cin >> K;
+		vector<long int>A(N);
+		vector<int>num_count(N, 0);
+		int i = 0, c = 0;
+		while (i<N) {
+			cin >> A[i];
+			i++;
+		}
+		i = 0;
+
+		while (i<N) {
+			num_count[A[i] - 1]++;
+			i++;
+		}
+
+		sort(num_count.begin(),num_count.end());
+		for (i = 0; i < N-K-1; i++) {
+			c++;
+		}
+
+/*
+		int j = 0;
+		while (j < K) {
+			int max = 0;
+			int index = 0;
+			i = 0;
+			while (i < N) {
+				if (max < num_count[i]) {
+					max = num_count[i];
+					index = i;
+				}
+				i++;
+			}
+			num_count[index] = 0;
+			j++;
+		}
+		
+		i = 0;
+		
+		while (i<N) {
+			c += num_count[i];
+			i++;
+		}
+		*/
+		printf("%d\n", c);
+
+	}
+
+};
---
+++ ABC081D.cpp
@@ -0,0 +1,48 @@
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <queue>
+#include <cmath>
+using namespace std;
+class ABC081D {
+	int N;
+	vector<long int>A;
+
+	int adder(vector<long int>a, int i) {
+		int min = 1000000, index = min;
+		for (int j = 0; j < N; j++) {
+			if (a[i] + a[j]< a[i + 1] && min>a[j]) {
+				min = a[j];
+				index = j;
+			}
+		}
+		a[i] += a[index];
+		return index;
+
+	}
+	ABC081D() {
+		cin >> N;
+		int i = 0, c = 0;
+		while (i < N) {
+			int num;
+			cin >> num;
+			A.push_back(num);
+			i++;
+		}
+
+		i = 0;
+
+	L:
+		bool end = true;
+		for (int j = 0; j < N - 1; j++) {
+			if (A[j] > A[j + 1]) {
+				printf("%d %d\n ", adder(A, j), j);
+				end = false;
+			}
+		}
+		if (!end)goto L;
+	}
+};
